e8913b2390ccbcaf7603923d0e2fc41373b75037
hanr119 <70984148+hanr119@users.noreply.github.com>
2021-05-01 21:50:19 -0400

    issue with toggle visibility

--- script.js
+++ script.js
@@ -53,15 +53,72 @@ function consoleLogStories() {
 function showStories() {
   console.log("showStories()");
   stories.forEach((story) => {
+      // var storyTitle = document.createElement("h1");
+      // storyTitle.innerText = story.fields.title;
+      // document.body.append(storyTitle);
+
+      // var nameOfAuthor = document.createElement("p");
+      // nameOfAuthor.innerText = story.fields.author;
+      // document.body.append(nameOfAuthor);
+
+      // var storyCover = document.createElement("img");
+      // storyCover.src = story.fields.cover[0].url;
+      // document.body.append(storyCover);
+
+      // creating a new div container
+      // defining space for story info
+      var storyContainer = document.createElement("div");
+      storyContainer.classList.add("story-container");
+      document.querySelector(".container").append(storyContainer);
+      
+      // add story titles to story container
       var storyTitle = document.createElement("h1");
+      storyTitle.classList.add("story-title");
       storyTitle.innerText = story.fields.title;
-      document.body.append(songTitle);
+      storyContainer.append(storyTitle);
 
-      var nameOfAuthor = document.createElement("p");
-      nameOfAuthor.innerText = story.fields.artist;
-      document.body.append(nameOfAuthor);
+      // add authors to story container
+      var storyAuthor = document.createElement("h1");
+      storyAuthor.classList.add("story-author");
+      storyAuthor.innerText = story.fields.author;
+      storyContainer.append(storyAuthor);
 
+      // add summary to story container
+      storySummary = document.createElement("p");
+      storySummary.classList.add("story-summary");
+      storySummary.innerText = story.fields.summary;
+      storyContainer.append(storySummary);
+
+      // add image to story container
       var storyCover = document.createElement("img");
-      songImage.src = story.fields.cover[0].url;
-  });
+      storyCover.classList.add("story-cover");
+      storyCover.src = story.fields.cover[0].url;
+      storyContainer.append(storyCover);
+
+      // add event listener
+      // when user clicks on story container, image and cover will toggle visibility
+      storyContainer.addEventListener("click", function(){
+          storySummary.classList.toggle("active");
+          storyContainer.classList.toggle("active");
+      })
+
+      // get type field from airtable
+      // loop through the array and add each genre as a class to the story container
+      var storyType = story.fields.type;
+      storyType.forEach(function(type){
+          storyContainer.classList.add(type);
+      })
+
+      // add event listener to filter to add active class to story
+
+      var filterDarkHumor = document.querySelector.apply(".dark-humor");
+      filterDarkHumor.addEventListener("click", function(){
+          if (storyContainer.classList.contains("dark-humor")) {
+            storyContainer.style.background = "#FFFFFF30";
+          } else {
+            storyContainer.style.background = "black"
+          }
+      })
+      
+    });
 }
